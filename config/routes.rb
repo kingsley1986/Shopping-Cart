04ce536a5c5ab39d1c43f5c34bf6c10220740b4d
kingsley <chukwumakingsley1@gmail.com>
2016-03-05 18:06:05 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
+
+  resources :products
+  root 'page#home'
+  
+  get 'page/about'
+
+  get 'page/faqs'
+
+  get 'page/contact'
+
+end
