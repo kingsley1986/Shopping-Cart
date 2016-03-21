afc3b73adc488fda1833feba9677f59fda58df16
kingsley <chukwumakingsley1@gmail.com>
2016-03-21 20:50:47 +0200

    keyword search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+def self.search(search)
+  where("title LIKE ?", "%#{search}%") 
+end
+
 has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
