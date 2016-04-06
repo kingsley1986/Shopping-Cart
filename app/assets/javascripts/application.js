430b26a60557de209fa7bc4eca5d2b6641d8b78e
kingsley <chukwumakingsley1@gmail.com>
2016-04-06 20:55:40 +0200

    removed require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,3 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
-//= require chosen-jquery
-//= require country_state_select
-//= require_tree .
